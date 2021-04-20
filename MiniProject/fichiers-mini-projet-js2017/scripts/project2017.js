e7418d19c62d517d17133100298e6f2d5bd3e4c3
mwone472 <mamadouwone12345@gmail.com>
2021-04-20 19:48:12 +0000

    20-04-2020

--- MiniProject/fichiers-mini-projet-js2017/scripts/project2017.js
+++ MiniProject/fichiers-mini-projet-js2017/scripts/project2017.js
@@ -15,6 +15,8 @@ var total = 0;
 // function called when page is loaded, it performs initializations 
 var init = function() {
     createShop();
+    getItemInCard();
+    deleteItem();
 
     // TODO : add other initializations to achieve if you think it is required
 }
@@ -55,9 +57,9 @@ var createProduct = function(product, index) {
     img = document.createElement('img')
     img.src = product.image
     figure.appendChild(img)
-        // block.appendChild(figure);
-        // /!\ should add the figure of the product... does not work yet... /!\ 
-    block.appendChild(createFigureBlock(product).appendChild(figure));
+    block.appendChild(figure);
+    // /!\ should add the figure of the product... does not work yet... /!\ 
+    // block.appendChild(createFigureBlock(product).appendChild(figure));
 
     // build and add the div.description part of 'block' 
     block.appendChild(createBlock("div", product.description, "description"));
@@ -113,7 +115,7 @@ var createOrderControlBlock = function(index) {
     // add control to control as its child
     control.appendChild(button);
     input.addEventListener('change', () => {
-        console.log(input.value);
+        // console.log(input.value);
         if (input.value > 0) {
             button.disabled = false;
         } else {
@@ -121,13 +123,58 @@ var createOrderControlBlock = function(index) {
         }
     })
     button.addEventListener('click', () => {
-        console.log(document.getElementById(index + '-product'));
+        addToCard(document.getElementById(index + '-product'), index,
+            input.value, catalog[index].price
+        );
+        location.reload();
     });
     // the built control div node is returned
     return control;
 }
 
 
+var addToCard = (product, index, price, quantity) => {
+    let allChild = product.children;
+    let container = document.getElementById("achats")
+    let block = document.createElement("div");
+    block.id = index + "-achat";
+    block.className = "achat";
+
+
+    block.appendChild(createBlock('figure', allChild[1].innerHTML));
+    block.appendChild(createBlock('h4', allChild[0].innerHTML));
+    block.appendChild(createBlock('div', price, 'quantite'));
+    block.appendChild(createBlock('div', quantity, 'prix'))
+    let remove = createBlock('div', '', 'controle')
+    let btn = createBlock('button', '', 'retirer');
+    btn.id = index + '-remove';
+    remove.appendChild(btn);
+    remove.addEventListener('click', () => {
+        console.log('click');
+    });
+    block.appendChild(remove);
+    let myDiv = createBlock('div', '');
+    myDiv.appendChild(block)
+    console.log(myDiv);
+    localStorage.setItem(allChild[0].innerHTML, myDiv.innerHTML)
+
+}
+
+var getItemInCard = () => {
+    let card = document.getElementById("achats")
+    console.log(card);
+    if (localStorage.length != 0) {
+        catalog.forEach(element => {
+            if (localStorage[element.name]) {
+                let content = localStorage.getItem(element.name);
+                // console.log(content);
+                achats.innerHTML += `${content}`;
+            }
+        });
+    }
+}
+
+// getItemInCard()
 /*
  * create and return the figure block for this product
  * see the static version of the project to know what the <figure> should be
@@ -138,5 +185,24 @@ var createOrderControlBlock = function(index) {
 var createFigureBlock = function(product) {
     // this is absolutely not the correct answer !
     // TODO 
-    return createBlock("figure", "");
+    let figure = document.createElement('figure')
+    img = document.createElement('img')
+    img.src = product.image
+    figure.appendChild(img)
+    return createBlock("figure", "", "");
+}
+
+
+var deleteItem = () => {
+    let card = document.querySelectorAll(".achat");
+    console.log(card);
+    for (let i = 0; i < card.length; i++) {
+        const element = card[i];
+        // console.log(element.children);
+        element.children[4].addEventListener('click', () => {
+            localStorage.removeItem(element.children[1].innerHTML);
+            location.reload();
+        })
+
+    }
 }
